755f1ddc93cf2491ca41ec97acabfdd100d012f4
gspetillo <gspetillo@gmail.com>
2020-09-08 21:10:42 -0300

    Add Carousel Lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mpsp_app/screens/home_screen.dart';
+import 'package:mpsp_app/screens/login_screen.dart';
 import 'package:mpsp_app/screens/register_screen.dart';
 
 void main() {
@@ -16,9 +17,11 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.red,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      initialRoute: '/',
+      initialRoute: '/login',
       routes: {
         "/": (context) => RegisterScreen(),
+        "/home": (context) => HomeScreen(),
+        "/login": (context) => LoginScreen(),
       },
     );
   }
