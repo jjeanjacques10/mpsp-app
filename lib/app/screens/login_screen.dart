f99d39d9da36e9b5992f09bc722e82b3d731e813
gspetillo <gspetillo@gmail.com>
2020-10-06 00:39:38 -0300

    Login Screen White Background

--- lib/app/screens/login_screen.dart
+++ lib/app/screens/login_screen.dart
@@ -20,6 +20,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
         child: Container(
           color: Colors.white,
