4762b8fc0b83383fb6b5e931f7eab55e92910ab4
gspetillo <gspetillo@gmail.com>
2020-10-14 23:54:33 -0300

    History Screen Layout

--- lib/app/screens/historic_Screen.dart
+++ lib/app/screens/historic_Screen.dart
@@ -16,15 +16,125 @@ class _HistoricScreenState extends State<HistoricScreen> {
         centerTitle: true,
         elevation: 0.0,
       ),
-      body: SingleChildScrollView(
-        child: Shimmer.fromColors(
-          highlightColor: Color.fromRGBO(64, 75, 96, .1),
-          baseColor: Colors.white,
-          period: Duration(milliseconds: 4000),
-          child: Container(
-            height: 18,
-            width: 40,
-            color: Color.fromRGBO(64, 75, 96, .1),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    "[22/11/2020 15:43] Maria Paula: ",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text("Lorem ipsum dollor"),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Text(
+                    "[22/11/2020 15:43] Maria Paula: ",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text("Lorem ipsum dollor"),
+                ],
+              ),
+              SizedBox(height: 10),Row(
+                children: [
+                  Text(
+                    "[22/11/2020 15:43] Maria Paula: ",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text("Lorem ipsum dollor"),
+                ],
+              ),
+              SizedBox(height: 10),Row(
+                children: [
+                  Text(
+                    "[22/11/2020 15:43] Maria Paula: ",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text("Lorem ipsum dollor"),
+                ],
+              ),
+              SizedBox(height: 10),Row(
+                children: [
+                  Text(
+                    "[22/11/2020 15:43] Maria Paula: ",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text("Lorem ipsum dollor"),
+                ],
+              ),
+              SizedBox(height: 10),Row(
+                children: [
+                  Text(
+                    "[22/11/2020 15:43] Maria Paula: ",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text("Lorem ipsum dollor"),
+                ],
+              ),
+              SizedBox(height: 10),Row(
+                children: [
+                  Text(
+                    "[22/11/2020 15:43] Maria Paula: ",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text("Lorem ipsum dollor"),
+                ],
+              ),
+              SizedBox(height: 10),Row(
+                children: [
+                  Text(
+                    "[22/11/2020 15:43] Maria Paula: ",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text("Lorem ipsum dollor"),
+                ],
+              ),
+              SizedBox(height: 10),Row(
+                children: [
+                  Text(
+                    "[22/11/2020 15:43] Maria Paula: ",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text("Lorem ipsum dollor"),
+                ],
+              ),
+              SizedBox(height: 10),Row(
+                children: [
+                  Text(
+                    "[22/11/2020 15:43] Maria Paula: ",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text("Lorem ipsum dollor"),
+                ],
+              ),
+              SizedBox(height: 10),
+            ],
           ),
         ),
       ),
